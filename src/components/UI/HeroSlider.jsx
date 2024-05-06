40b44e33597aea3e8ee449141b92691e74eeb0fa
RajeshVarma22 <rajeshvarma2203@gmail.com>
2024-05-06 12:00:01 +0530

    Changed layout

--- src/components/UI/HeroSlider.jsx
+++ src/components/UI/HeroSlider.jsx
@@ -6,16 +6,18 @@ import { Link } from "react-router-dom";
 import "../../styles/hero-slider.css";
 
 const HeroSlider = () => {
-
   return (
     <div className="slider__item slider__item-01 mt0">
       <Container>
         <div className="slider__content ">
-          <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-          <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
+          <h4 className="text-light mb-3">Unlock Exclusive Deals</h4>
+          <h1 className="text-light mb-4">
+            Explore our wide range of vehicles available for rent at unbeatable
+            prices.
+          </h1>
 
           <button className="btn reserve__btn mt-4">
-            <Link to="/cars">Reserve Now</Link>
+            <Link to="/cars">Browse Vehicles</Link>
           </button>
         </div>
       </Container>
